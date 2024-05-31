3ac855515135937e4027375bc68ba608fce4c584
Hehe <112867518+Ze-r000@users.noreply.github.com>
2024-05-31 23:29:33 +0800

    Add files via upload

---
+++ dataset_splitter.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+import random
+
+# Define the paths
+source_folder = "Queenie Amargo"
+test_folder = "test"
+train_folder = "train"
+validation_folder = "validation"
+
+# Define the number of images for each category
+num_test_images = 20
+num_train_images = 180
+num_validation_images = 50
+
+# List all image files in the source folder
+image_files = [f for f in os.listdir(source_folder) if f.endswith(".jpg")]
+
+# Shuffle the list to randomize the selection
+random.shuffle(image_files)
+
+# Copy images to test folder
+for img in image_files[:num_test_images]:
+    img_path = os.path.join(source_folder, img)
+    xml_path = os.path.join(source_folder, img.replace(".jpg", ".xml"))
+    shutil.copy(img_path, test_folder)
+    shutil.copy(xml_path, test_folder)
+
+# Copy images to train folder
+for img in image_files[num_test_images:num_test_images+num_train_images]:
+    img_path = os.path.join(source_folder, img)
+    xml_path = os.path.join(source_folder, img.replace(".jpg", ".xml"))
+    shutil.copy(img_path, train_folder)
+    shutil.copy(xml_path, train_folder)
+
+# Copy images to validation folder
+for img in image_files[num_test_images+num_train_images:num_test_images+num_train_images+num_validation_images]:
+    img_path = os.path.join(source_folder, img)
+    xml_path = os.path.join(source_folder, img.replace(".jpg", ".xml"))
+    shutil.copy(img_path, validation_folder)
+    shutil.copy(xml_path, validation_folder)
---
+++ readMe.txt
@@ -0,0 +1,9 @@
+0. I recommend to edit the dataset-splitter.py by using args to call the functions and folders in terminal.
+	- For now, just locate the and edit the script on where you can locate your folders.
+1. Open File dataset_splitter.py
+	- change directory of the train, test, validation and dataset locations
+2. Activate env
+	- conda activate caffee
+3. Run script
+	- python dataset_splitter.py
+	
